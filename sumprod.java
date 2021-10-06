5fd322e1ad271f05103d58e5d05372ebda7caa18
FayizaMushtaq <90772586+FayizaMushtaq@users.noreply.github.com>
2021-10-06 12:01:31 +0530

    Add files via upload

---
+++ sumprod.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;	
+
+public class sumprod {
+
+	public static void main(String args[]) {
+	  Scanner in = new Scanner(System.in); 
+	 
+	  System.out.print("Enter Number: ");
+	  int number = in.nextInt();
+	  int remainder, temp, sum=0, product=1;
+	  temp = number;
+	  while (temp!=0){
+		  remainder = temp % 10;
+		  sum = sum + remainder;
+		  product = product * remainder;
+		  temp = temp / 10;
+	  }
+	  System.out.println("Sum of digits of Number '"+number+"'': "+sum);
+	  System.out.println("Product of digits of Number '"+number+"'': "+product);
+	}
+ } 
