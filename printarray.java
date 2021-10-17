f433f92a6b948aa3db7a5b5b9af2ad634714eb79
FayizaMushtaq <90772586+FayizaMushtaq@users.noreply.github.com>
2021-10-17 12:48:38 +0530

    Add files via upload

---
+++ printarray.java
@@ -0,0 +1,11 @@
+//java program to print the elements of array//
+public class printarray
+{
+public static void main(String[] args)
+{
+int[]arr=new int[]{1,2,3,4,5,6,7};
+System.out.println("elements of given array are:");
+for(int i=0;i<arr.length;i++)
+{System.out.println(arr[i]+" ");
+}
+}}
---
+++ printreverse.java
@@ -0,0 +1,11 @@
+//java program to print the reverse of array//
+public class printreverse
+{
+public static void main(String[] args)
+{
+int[]arr=new int[]{1,2,3,4,5,6,7,8};
+System.out.println("reverse of given array is:");
+for(int i=7;i>=0;i--)
+{System.out.println(arr[i]+" ");
+}
+}}
